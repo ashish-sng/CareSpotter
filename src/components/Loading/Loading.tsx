7ed41eac04cd18140d872e4fd58cff24c20c499a
Ashish Singh <ashishsingh.1899@gmail.com>
2023-07-22 22:13:32 +0530

    added loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,20 @@
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loader">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
