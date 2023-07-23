515795341e0385f1f24ea59ac4ae14a8d8b4b4fd
Ashish Singh <ashishsingh.1899@gmail.com>
2023-07-23 11:52:51 +0530

    tweaks

--- src/baseurl.tsx
+++ src/baseurl.tsx
@@ -1,3 +1,5 @@
 const BASEURL: string = "https://carespotter.onrender.com";
+// const BASEURL: string = "http://localhost:4000";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export default BASEURL;
